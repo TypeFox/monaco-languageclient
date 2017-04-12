87df47ae8a344aed038efb255325e8d0b666f54f
akosyakov <anton.kosyakov@typefox.io>
2017-04-12 12:59:04 +0200

    minor refactoring

--- src/languages.ts
+++ src/languages.ts
@@ -1,10 +1,30 @@
 import globToRegExp from "glob-to-regexp";
-import { Languages, DiagnosticCollection, CompletionItemProvider } from "vscode-languageclient/lib/services";
+import { Languages, DiagnosticCollection, CompletionItemProvider, DocumentIdentifier } from "vscode-languageclient/lib/services";
 import { CompletionClientCapabilities, DocumentFilter, DocumentSelector } from 'vscode-languageclient/lib/protocol';
 import { MonacoDiagnosticCollection } from './diagnostic-collection';
 import { ProtocolToMonacoConverter, MonacoToProtocolConverter } from './converter';
 import { DisposableCollection, Disposable } from "./disposable";
 import Uri = monaco.Uri;
+
+export interface MonacoModelIdentifier {
+    uri: Uri;
+    languageId: string;
+}
+
+export namespace MonacoModelIdentifier {
+    export function fromDocument(document: DocumentIdentifier): MonacoModelIdentifier {
+        return {
+            uri: Uri.parse(document.uri),
+            languageId: document.languageId
+        }
+    }
+    export function fromModel(model: monaco.editor.IReadOnlyModel): MonacoModelIdentifier {
+        return {
+            uri: model.uri,
+            languageId: model.getModeId()
+        }
+    }
+}
 
 export class MonacoLanguages implements Languages {
 
@@ -17,16 +37,10 @@ export class MonacoLanguages implements Languages {
     constructor(
         protected readonly p2m: ProtocolToMonacoConverter,
         protected readonly m2p: MonacoToProtocolConverter
-    ) {}
+    ) { }
 
-    match(selector: DocumentSelector, document: {
-        uri: string;
-        languageId: string;
-    }): boolean {
-        return this.matchModel(selector, {
-            uri: Uri.parse(document.uri),
-            languageId: document.languageId
-        });
+    match(selector: DocumentSelector, document: DocumentIdentifier): boolean {
+        return this.matchModel(selector, MonacoModelIdentifier.fromDocument(document));
     }
 
     createDiagnosticCollection?(name?: string): DiagnosticCollection {
@@ -39,7 +53,7 @@ export class MonacoLanguages implements Languages {
             providers.push(monaco.languages.registerCompletionItemProvider(language.id, {
                 triggerCharacters,
                 provideCompletionItems: (model, position, token) => {
-                    if (!this.matchModel(selector, { uri: model.uri, languageId: model.getModeId() })) {
+                    if (!this.matchModel(selector, MonacoModelIdentifier.fromModel(model))) {
                         return [];
                     }
                     const params = this.m2p.asTextDocumentPositionParams(model, position)
@@ -50,10 +64,7 @@ export class MonacoLanguages implements Languages {
         return providers;
     }
 
-    protected matchModel(selector: string | DocumentFilter | DocumentSelector, model: {
-        uri: Uri;
-        languageId: string;
-    }): boolean {
+    protected matchModel(selector: string | DocumentFilter | DocumentSelector, model: MonacoModelIdentifier): boolean {
         if (Array.isArray(selector)) {
             return selector.findIndex(filter => this.matchModel(filter, model)) !== -1;
         }
--- src/workspace.ts
+++ src/workspace.ts
@@ -5,11 +5,14 @@ import { Workspace, TextDocumentDidChangeEvent } from "vscode-languageclient/lib
 import IModel = monaco.editor.IModel;
 
 export class MonacoWorkspace implements Workspace {
+
     protected _rootUri: string | null = null;
+
     protected readonly documents = new Map<string, TextDocument>();
     protected readonly onDidOpenTextDocumentEmitter = new Emitter<TextDocument>();
     protected readonly onDidCloseTextDocumentEmitter = new Emitter<TextDocument>();
     protected readonly onDidChangeTextDocumentEmitter = new Emitter<TextDocumentDidChangeEvent>();
+
     constructor(
         protected readonly m2p: MonacoToProtocolConverter) {
         for (const model of monaco.editor.getModels()) {
@@ -18,9 +21,11 @@ export class MonacoWorkspace implements Workspace {
         monaco.editor.onDidCreateModel(model => this.addModel(model));
         monaco.editor.onWillDisposeModel(model => this.removeModel(model));
     }
+
     get rootUri() {
         return this._rootUri;
     }
+
     protected removeModel(model: IModel): void {
         const uri = model.uri.toString();
         const document = this.documents.get(uri);
@@ -29,35 +34,47 @@ export class MonacoWorkspace implements Workspace {
             this.onDidCloseTextDocumentEmitter.fire(document);
         }
     }
+
     protected addModel(model: IModel): void {
         const uri = model.uri.toString();
-        this.onDidOpenTextDocumentEmitter.fire(this.setModel(uri, model))
-        model.onDidChangeContent(event => {
-            const textDocument = this.setModel(uri, model);
-            const range = this.m2p.asRange(event.range);
-            const rangeLength = event.rangeLength;
-            const text = event.text;
-            this.onDidChangeTextDocumentEmitter.fire({
-                textDocument,
-                contentChanges: [{ range, rangeLength, text }]
-            });
+        const document = this.setModel(uri, model);
+        this.onDidOpenTextDocumentEmitter.fire(document)
+        model.onDidChangeContent(event =>
+            this.onDidChangeContent(uri, model, event)
+        );
+    }
+
+    protected onDidChangeContent(uri: string, model: IModel, event: monaco.editor.IModelContentChangedEvent2) {
+        const textDocument = this.setModel(uri, model);
+        const range = this.m2p.asRange(event.range);
+        const rangeLength = event.rangeLength;
+        const text = event.text;
+        this.onDidChangeTextDocumentEmitter.fire({
+            textDocument,
+            contentChanges: [{ range, rangeLength, text }]
         });
     }
+
     protected setModel(uri: string, model: IModel): TextDocument {
         const document = TextDocument.create(uri, model.getModeId(), model.getVersionId(), model.getValue());
         this.documents.set(uri, document);
         return document;
     }
+
     get textDocuments(): TextDocument[] {
         return Array.from(this.documents.values());
     }
+
     get onDidOpenTextDocument(): Event<TextDocument> {
         return this.onDidOpenTextDocumentEmitter.event;
     }
+
     get onDidCloseTextDocument(): Event<TextDocument> {
         return this.onDidCloseTextDocumentEmitter.event;
     }
+
     get onDidChangeTextDocument(): Event<TextDocumentDidChangeEvent> {
         return this.onDidChangeTextDocumentEmitter.event;
     }
+
 }
