5f5fe4f0ba999baf460a51d6003b18f07c9a59d6
Christian Dietrich <christian.dietrich@itemis.de>
2017-09-22 08:11:34 +0200

    passing Options to createMonacoServices to allow a rootUri being set #31
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- src/services.ts
+++ src/services.ts
@@ -9,13 +9,17 @@ import { MonacoLanguages } from "./languages";
 import { MonacoWorkspace } from "./workspace";
 import { ConsoleWindow } from "./console-window";
 
-export function createMonacoServices(editor: monaco.editor.IStandaloneCodeEditor): BaseLanguageClient.IServices {
+export function createMonacoServices(editor: monaco.editor.IStandaloneCodeEditor, options: MonacoServicesOptions = {}): BaseLanguageClient.IServices {
     const m2p = new MonacoToProtocolConverter();
     const p2m = new ProtocolToMonacoConverter();
     return {
         commands: new MonacoCommands(editor),
         languages: new MonacoLanguages(p2m, m2p),
-        workspace: new MonacoWorkspace(p2m, m2p),
+        workspace: new MonacoWorkspace(p2m, m2p, options.rootUri),
         window: new ConsoleWindow()
     }
+}
+
+export interface MonacoServicesOptions {
+    rootUri?: string
 }
