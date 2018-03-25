44a4570d7a9bee8495d79c1d4c4b9f713ee2b1e8
Remy Suen <remy.suen@gmail.com>
2018-03-25 20:23:50 +0900

    Create a browser-only example
    As the Monaco Editor can be opened in a simple HTML page, it should
    also be possible to leverage the monaco-languageclient in the same
    setup. TO demonstrate this, a new example has been created in the
    examples/browser folder.
    
    Instead of using the actual language server for the editor features,
    the language service that is backing the language server must be used
    instead. The types from the language service will be converted to and
    from Monaco types with the help of the converters provided by
    monaco-languageclient.
    
    Since this is intended to be run in a simple HTML page without a
    server serving the content, only language services written in
    JavaScript may be used as that is the programming language that
    browsers speak.
    
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

---
+++ examples/browser/src/client.ts
@@ -0,0 +1,136 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) 2018 TypeFox GmbH (http://www.typefox.io). All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+import { getLanguageService, TextDocument } from "vscode-json-languageservice";
+import { MonacoToProtocolConverter, ProtocolToMonacoConverter } from 'monaco-languageclient';
+
+const LANGUAGE_ID = 'json';
+const MODEL_URI = 'inmemory://model.json'
+const MONACO_URI = monaco.Uri.parse(MODEL_URI);
+
+// register the JSON language with Monaco
+monaco.languages.register({
+    id: LANGUAGE_ID,
+    extensions: ['.json', '.bowerrc', '.jshintrc', '.jscsrc', '.eslintrc', '.babelrc'],
+    aliases: ['JSON', 'json'],
+    mimetypes: ['application/json'],
+});
+
+// create the Monaco editor
+const value = `{
+    "$schema": "http://json.schemastore.org/coffeelint",
+    "line_endings": "unix"
+}`;
+monaco.editor.create(document.getElementById("container")!, {
+    model: monaco.editor.createModel(value, LANGUAGE_ID, MONACO_URI),
+    glyphMargin: true,
+    lightbulb: {
+        enabled: true
+    }
+});
+
+function getModel(): monaco.editor.IModel {
+    return monaco.editor.getModel(MONACO_URI);
+}
+
+function createDocument(model: monaco.editor.IReadOnlyModel) {
+    return TextDocument.create(MODEL_URI, model.getModeId(), model.getVersionId(), model.getValue());
+}
+
+function resovleSchema(url: string): Promise<string> {
+    const promise = new Promise<string>((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = () => resolve(xhr.responseText);
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.open("GET", url, true);
+        xhr.send();
+    });
+    return promise;
+}
+
+const m2p = new MonacoToProtocolConverter();
+const p2m = new ProtocolToMonacoConverter();
+const jsonService = getLanguageService({
+    schemaRequestService: resovleSchema
+});
+const pendingValidationRequests = new Map<string, number>();
+
+monaco.languages.registerCompletionItemProvider(LANGUAGE_ID, {
+    provideCompletionItems(model, position, token): monaco.languages.CompletionItem[] | Thenable<monaco.languages.CompletionItem[]> | monaco.languages.CompletionList | Thenable<monaco.languages.CompletionList> {
+        const document = createDocument(model);
+        const jsonDocument = jsonService.parseJSONDocument(document);
+        return jsonService.doComplete(document, m2p.asPosition(position.lineNumber, position.column), jsonDocument).then((list) => {
+            return p2m.asCompletionResult(list);
+        });
+    },
+
+    resolveCompletionItem(item, token): monaco.languages.CompletionItem | Thenable<monaco.languages.CompletionItem> {
+        return jsonService.doResolve(m2p.asCompletionItem(item)).then((result) => {
+            return p2m.asCompletionItem(result);
+        });
+    }
+});
+
+monaco.languages.registerDocumentRangeFormattingEditProvider(LANGUAGE_ID, {
+    provideDocumentRangeFormattingEdits(model, range, options, token): monaco.languages.TextEdit[] | Thenable<monaco.languages.TextEdit[]> {
+        const document = createDocument(model);
+        const edits = jsonService.format(document, m2p.asRange(range), m2p.asFormattingOptions(options));
+        return p2m.asTextEdits(edits);
+    }
+});
+
+monaco.languages.registerDocumentSymbolProvider(LANGUAGE_ID, {
+    provideDocumentSymbols(model, token): monaco.languages.SymbolInformation[] | Thenable<monaco.languages.SymbolInformation[]> {
+        const document = createDocument(model);
+        const jsonDocument = jsonService.parseJSONDocument(document);
+        return p2m.asSymbolInformations(jsonService.findDocumentSymbols(document, jsonDocument));
+    }
+});
+
+monaco.languages.registerHoverProvider(LANGUAGE_ID, {
+    provideHover(model, position, token): monaco.languages.Hover | Thenable<monaco.languages.Hover> {
+        const document = createDocument(model);
+        const jsonDocument = jsonService.parseJSONDocument(document);
+        return jsonService.doHover(document, m2p.asPosition(position.lineNumber, position.column), jsonDocument).then((hover) => {
+            return p2m.asHover(hover);
+        });
+    }
+});
+
+getModel().onDidChangeContent((event) => {
+    validate();
+});
+
+function validate(): void {
+    const document = createDocument(getModel());
+    cleanPendingValidation(document);
+    pendingValidationRequests.set(document.uri, setTimeout(() => {
+        pendingValidationRequests.delete(document.uri);
+        doValidate(document);
+    }));
+}
+
+function cleanPendingValidation(document: TextDocument): void {
+    const request = pendingValidationRequests.get(document.uri);
+    if (request !== undefined) {
+        clearTimeout(request);
+        pendingValidationRequests.delete(document.uri);
+    }
+}
+
+function doValidate(document: TextDocument): void {
+    if (document.getText().length === 0) {
+        cleanDiagnostics();
+        return;
+    }
+    const jsonDocument = jsonService.parseJSONDocument(document);
+    jsonService.doValidation(document, jsonDocument).then((diagnostics) => {
+        const markers = diagnostics.map(diagnostic => p2m.asMarker(diagnostic));
+        monaco.editor.setModelMarkers(getModel(), 'default', markers);
+    });
+}
+
+function cleanDiagnostics(): void {
+    monaco.editor.setModelMarkers(monaco.editor.getModel(MONACO_URI), 'default', []);
+}
