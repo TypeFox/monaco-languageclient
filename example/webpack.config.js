ff1154af5c23fb96d759ff0e0de10b53ff91e375
Loïc Mangeonjean <loic@codingame.com>
2021-02-08 10:41:57 +0100

    Prevent warning flood in the console when building

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -59,7 +59,9 @@ if (process.env['NODE_ENV'] === 'production') {
             rules: [{
                 test: /\.js$/,
                 enforce: 'pre',
-                loader: 'source-map-loader'
+                loader: 'source-map-loader',
+                // These modules seems to have broken sourcemaps, exclude them to prevent an error flood in the logs
+                exclude: [/vscode-jsonrpc/, /vscode-languageclient/, /vscode-languageserver-protocol/]
             }]
         }
     })
