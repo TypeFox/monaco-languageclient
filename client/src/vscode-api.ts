42ab0b44178a1c41977e52739e5b0faebb28670b
Loïc Mangeonjean <loic@codingame.com>
2022-03-21 09:41:04 +0100

    Use `import type` syntax to prevent importing something else than types

--- client/src/vscode-api.ts
+++ client/src/vscode-api.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import * as vscode from "vscode";
+import type * as vscode from "vscode";
 import { URI } from "vscode-uri"
 import { Disposable } from "./disposable";
 import {
