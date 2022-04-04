4138469d9dfb23115efd4bafb37be4a0a2e7829d
Kai Salmen <kai.salmen@typefox.io>
2022-04-04 13:38:22 +0200

    Move all examples to new locations
    - client keeps original typescript config for now
    - Test vite in examples/client

--- packages/examples/client/src/client.ts
+++ packages/examples/client/src/client.ts
@@ -2,14 +2,10 @@
  * Copyright (c) 2018 TypeFox GmbH (http://www.typefox.io). All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-import { listen } from '@codingame/monaco-jsonrpc';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-import {
-    MonacoLanguageClient, MessageConnection, CloseAction, ErrorAction,
-    MonacoServices, createConnection
-} from 'monaco-languageclient';
+import { MonacoLanguageClient, MessageConnection, CloseAction, ErrorAction, MonacoServices, createConnection } from 'monaco-languageclient';
+import { listen } from '@codingame/monaco-jsonrpc';
 import normalizeUrl from 'normalize-url';
-const ReconnectingWebSocket = require('reconnecting-websocket');
 
 // register Monaco languages
 monaco.languages.register({
@@ -36,12 +32,15 @@ monaco.editor.create(document.getElementById("container")!, {
 MonacoServices.install(monaco);
 
 // create the web socket
-const url = createUrl('/sampleServer')
-const webSocket = createWebSocket(url);
+const url = createUrl('localhost', 3000, '/sampleServer')
+const webSocket = new WebSocket(url);
+
 // listen when the web socket is opened
 listen({
     webSocket,
     onConnection: connection => {
+        console.log('Connected to: ' + url);
+
         // create and start the language client
         const languageClient = createLanguageClient(connection);
         const disposable = languageClient.start();
@@ -70,19 +69,7 @@ function createLanguageClient(connection: MessageConnection): MonacoLanguageClie
     });
 }
 
-function createUrl(path: string): string {
+function createUrl(hostname: string, port: number, path: string): string {
     const protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-    return normalizeUrl(`${protocol}://${location.host}${location.pathname}${path}`);
-}
-
-function createWebSocket(url: string): WebSocket {
-    const socketOptions = {
-        maxReconnectionDelay: 10000,
-        minReconnectionDelay: 1000,
-        reconnectionDelayGrowFactor: 1.3,
-        connectionTimeout: 10000,
-        maxRetries: Infinity,
-        debug: false
-    };
-    return new ReconnectingWebSocket(url, [], socketOptions);
+    return normalizeUrl(`${protocol}://${hostname}:${port}${path}`);
 }
