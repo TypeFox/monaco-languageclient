ffe6fd571f0be1fb9079f318166da273ebd4bd61
Kai Salmen <kai.salmen@typefox.io>
2022-05-20 17:27:26 +0200

    Ensure vite dev starts with clearing the cache (for now)

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     },
     optimizeDeps: {
         // we need this as it is locally referenced/linked by the examples
-        // if it is regular dependency resoled from npmjs this is not required
+        // if it is regular dependency resolved from npmjs this is not required
         include: ['monaco-languageclient']
     },
     build: {
@@ -21,6 +21,9 @@ export default defineConfig({
                 client: path.resolve(__dirname, '/packages/examples/client/index.html'),
                 browser: path.resolve(__dirname, '/packages/examples/browser/index.html')
             }
+        },
+        commonjsOptions: {
+            include: [/monaco-languageclient/, /node_modules/]
         }
     },
     server: {
