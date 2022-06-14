1febee52904bcc99da93fbaa35968fe51f45ed10
Kai Salmen <kai.salmen@typefox.io>
2022-06-14 17:36:52 +0200

    Clean vite config and fix build

--- packages/client/src/monaco-language-client.ts
+++ packages/client/src/monaco-language-client.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 import {
-    BaseLanguageClient, MessageTransports, LanguageClientOptions
+    BaseLanguageClient, MessageTransports, LanguageClientOptions, CloseAction, ErrorAction
 } from "vscode-languageclient/lib/common/client";
 
 export * from 'vscode-languageclient/lib/common/client';
@@ -135,3 +135,5 @@ export namespace MonacoLanguageClient {
         connectionProvider: IConnectionProvider;
     }
 }
+
+export { CloseAction, ErrorAction }
