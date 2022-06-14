962be77226fe83545e8998d4865cf42ec65a323f
Loïc Mangeonjean <loic@codingame.com>
2022-06-14 15:31:37 +0200

    Remove unused files

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -3,12 +3,13 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 export * from './disposable';
-export * from './services';
-export * from './connection';
 export * from './monaco-language-client';
-export * from './monaco-commands';
 export * from './console-window';
-export * from './monaco-languages';
 export * from './monaco-workspace';
 export * from './monaco-services';
 export * from './monaco-converter';
+
+export {
+    Disposable, CancellationToken, Event, Emitter
+} from 'vscode-jsonrpc/lib/common/api';
+export * from 'vscode-languageserver-protocol/lib/common/api';
