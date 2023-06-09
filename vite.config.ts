fb812311f615d7b47d65fa3f1daaf316962c28b0
Kai Salmen <kai.salmen@typefox.io>
2023-06-09 20:31:02 +0200

    Remove unnecessary optimizeDeps

--- vite.config.ts
+++ vite.config.ts
@@ -23,35 +23,7 @@ export default defineConfig(() => {
             port: 8080,
             origin: 'http://localhost:8080'
         },
-        assetsInclude: ['**/*.wasm'],
-        optimizeDeps: [
-
-            'monaco-editor',
-            'vscode',
-            'vscode/extensions',
-            'vscode/services',
-            'vscode/monaco',
-            'vscode/service-override/model',
-            'vscode/service-override/editor',
-            'vscode/service-override/notifications',
-            'vscode/service-override/dialogs',
-            'vscode/service-override/configuration',
-            'vscode/service-override/keybindings',
-            'vscode/service-override/textmate',
-            'vscode/service-override/theme',
-            'vscode/service-override/languages',
-            'vscode/service-override/audioCue',
-            'vscode/service-override/views',
-            'vscode/service-override/quickaccess',
-            'vscode/service-override/debug',
-            'vscode/service-override/preferences',
-            'vscode/service-override/snippets',
-            'vscode/service-override/files',
-            'vscode/default-extensions/theme-defaults',
-            'vscode/default-extensions/javascript',
-            'vscode/default-extensions/typescript',
-            'vscode/default-extensions/json'
-        ]
+        assetsInclude: ['**/*.wasm']
     };
     return config;
 });
