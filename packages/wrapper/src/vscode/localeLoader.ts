a983c52b24042679f30619d9b63d7a1b23fbea19
undefined <i@undefined.moe>
2024-06-03 21:47:37 +0800

    chore: localeLoader: allow manually select which locale to load

--- packages/wrapper/src/vscode/localeLoader.ts
+++ packages/wrapper/src/vscode/localeLoader.ts
@@ -3,53 +3,54 @@
  * Licensed under the MIT License. See LICENSE in the package root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-export const initLocalLoader = async () => {
-    const locale = new URLSearchParams(window.location.search).get('locale');
-    const localeLoader: Partial<Record<string, () => Promise<void>>> = {
-        cs: async () => {
-            await import('@codingame/monaco-vscode-language-pack-cs');
-        },
-        de: async () => {
-            await import('@codingame/monaco-vscode-language-pack-de');
-        },
-        es: async () => {
-            await import('@codingame/monaco-vscode-language-pack-es');
-        },
-        fr: async () => {
-            await import('@codingame/monaco-vscode-language-pack-fr');
-        },
-        it: async () => {
-            await import('@codingame/monaco-vscode-language-pack-it');
-        },
-        ja: async () => {
-            await import('@codingame/monaco-vscode-language-pack-ja');
-        },
-        ko: async () => {
-            await import('@codingame/monaco-vscode-language-pack-ko');
-        },
-        pl: async () => {
-            await import('@codingame/monaco-vscode-language-pack-pl');
-        },
-        'pt-br': async () => {
-            await import('@codingame/monaco-vscode-language-pack-pt-br');
-        },
-        'qps-ploc': async () => {
-            await import('@codingame/monaco-vscode-language-pack-qps-ploc');
-        },
-        ru: async () => {
-            await import('@codingame/monaco-vscode-language-pack-ru');
-        },
-        tr: async () => {
-            await import('@codingame/monaco-vscode-language-pack-tr');
-        },
-        'zh-hans': async () => {
-            await import('@codingame/monaco-vscode-language-pack-zh-hans');
-        },
-        'zh-hant': async () => {
-            await import('@codingame/monaco-vscode-language-pack-zh-hant');
-        }
-    };
+const localeLoader: Partial<Record<string, () => Promise<void>>> = {
+    cs: async () => {
+        await import('@codingame/monaco-vscode-language-pack-cs');
+    },
+    de: async () => {
+        await import('@codingame/monaco-vscode-language-pack-de');
+    },
+    es: async () => {
+        await import('@codingame/monaco-vscode-language-pack-es');
+    },
+    fr: async () => {
+        await import('@codingame/monaco-vscode-language-pack-fr');
+    },
+    it: async () => {
+        await import('@codingame/monaco-vscode-language-pack-it');
+    },
+    ja: async () => {
+        await import('@codingame/monaco-vscode-language-pack-ja');
+    },
+    ko: async () => {
+        await import('@codingame/monaco-vscode-language-pack-ko');
+    },
+    pl: async () => {
+        await import('@codingame/monaco-vscode-language-pack-pl');
+    },
+    'pt-br': async () => {
+        await import('@codingame/monaco-vscode-language-pack-pt-br');
+    },
+    'qps-ploc': async () => {
+        await import('@codingame/monaco-vscode-language-pack-qps-ploc');
+    },
+    ru: async () => {
+        await import('@codingame/monaco-vscode-language-pack-ru');
+    },
+    tr: async () => {
+        await import('@codingame/monaco-vscode-language-pack-tr');
+    },
+    'zh-hans': async () => {
+        await import('@codingame/monaco-vscode-language-pack-zh-hans');
+    },
+    'zh-hant': async () => {
+        await import('@codingame/monaco-vscode-language-pack-zh-hant');
+    }
+};
 
+export const locales = Object.keys(localeLoader);
+
+export const initLocaleLoader = async (locale = new URLSearchParams(window.location.search).get('locale')) => {
     if (locale !== null) {
         const loader = localeLoader[locale];
         if (loader) {
@@ -59,3 +60,5 @@ export const initLocalLoader = async () => {
         }
     }
 };
+/** @deprecated use `initLocaleLoader` instead */
+export const initLocalLoader = initLocaleLoader;
