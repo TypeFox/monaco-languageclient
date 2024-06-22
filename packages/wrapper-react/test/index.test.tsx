fea135c06a864216f58ad4e8ed449e763ef1b19a
Vijay Ramamurthy <vrama628@gmail.com>
2024-06-22 12:35:41 -0500

    test

--- packages/wrapper-react/test/index.test.tsx
+++ packages/wrapper-react/test/index.test.tsx
@@ -27,4 +27,26 @@ describe('Test MonacoEditorReactComp', () => {
         await Promise.resolve();
         rerender(<MonacoEditorReactComp userConfig={userConfig} />);
     });
+
+    test('update onTextChanged', async () => {
+        const userConfig: UserConfig = {
+            wrapperConfig: {
+                editorAppConfig: {
+                    $type: 'extended',
+                    codeResources: {
+                        main: {
+                            text: '',
+                            fileExt: 'js'
+                        }
+                    }
+                }
+            },
+            loggerConfig: {
+                enabled: true,
+                debugEnabled: true,
+            }
+        };
+        const { rerender } = render(<MonacoEditorReactComp userConfig={userConfig} />);
+        await new Promise(resolve => rerender(<MonacoEditorReactComp userConfig={userConfig} onTextChanged={resolve} />));
+    });
 });
