72140a653d1d2b5f95d45b73b0806a57052ef272
adrian2024lsp <adrian2024lsp@gmail.com>
2024-10-08 13:38:04 +1300

    fix(angular): update to standalone for new angular convention

--- verify/angular/src/app/app.component.ts
+++ verify/angular/src/app/app.component.ts
@@ -3,24 +3,25 @@
  * Licensed under the MIT License. See LICENSE in the package root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import { AfterViewInit, Component } from '@angular/core';
-import { MonacoEditorLanguageClientWrapper } from 'monaco-editor-wrapper';
-import { jsonClientUserConfig } from 'monaco-languageclient-examples/json-client';
+import { AfterViewInit, Component } from "@angular/core";
+import { MonacoEditorLanguageClientWrapper } from "monaco-editor-wrapper";
+import { jsonClientUserConfig } from "monaco-languageclient-examples/json-client";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
+    standalone: true,
 })
 export class MonacoEditorComponent implements AfterViewInit {
-    title = 'angular-client';
+    title = "angular-client";
     initDone = false;
 
     async ngAfterViewInit(): Promise<void> {
         const wrapper = new MonacoEditorLanguageClientWrapper();
-        const htmlElement = document.getElementById('monaco-editor-root');
+        const htmlElement = document.getElementById("monaco-editor-root");
         try {
-            await wrapper.initAndStart(jsonClientUserConfig , htmlElement );
+            await wrapper.initAndStart(jsonClientUserConfig, htmlElement);
         } catch (e) {
             console.error(e);
         }
--- verify/angular/src/main.ts
+++ verify/angular/src/main.ts
@@ -3,10 +3,6 @@
  * Licensed under the MIT License. See LICENSE in the package root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch((err: Error) => {
-        console.error(err);
-    });
+import { bootstrapApplication } from "@angular/platform-browser";
+import { MonacoEditorComponent } from "./app/app.component";
+bootstrapApplication(MonacoEditorComponent);
