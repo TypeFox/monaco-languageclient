463a812a78e5d378d0f4793890b7807d477e3863
adrian2024lsp <adrian2024lsp@gmail.com>
2024-10-08 15:11:50 +1300

    fix(angular): fix ci

--- verify/angular/src/main.ts
+++ verify/angular/src/main.ts
@@ -3,6 +3,6 @@
  * Licensed under the MIT License. See LICENSE in the package root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import { bootstrapApplication } from "@angular/platform-browser";
-import { MonacoEditorComponent } from "./app/app.component";
+import { bootstrapApplication } from '@angular/platform-browser';
+import { MonacoEditorComponent } from './app/app.component';
 bootstrapApplication(MonacoEditorComponent);
