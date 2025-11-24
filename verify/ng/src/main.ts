f03e8eccc1ea944923c87ecc3be04f825c86343f
Kai Salmen <kai.salmen@typefox.io>
2025-11-24 14:30:13 +0100

    Test old webpack custom config

---
+++ verify/ng/src/main.ts
@@ -0,0 +1,8 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) 2023 TypeFox and others.
+ * Licensed under the MIT License. See LICENSE in the package root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { MonacoEditorComponent } from './app/app.component';
+bootstrapApplication(MonacoEditorComponent);
