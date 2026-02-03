79f5d959c122128864564ee683a379f0e9737965
Kai Salmen <kai.salmen@typefox.io>
2026-02-03 18:48:42 +0100

    Re-enable plugin for configure-response-headers

--- vite.config.ts
+++ vite.config.ts
@@ -70,19 +70,19 @@ export const definedViteConfig = defineConfig({
         ]
     },
     plugins: [
-        // {
-        //     // For the *-language-features extensions which use SharedArrayBuffer
-        //     name: 'configure-response-headers',
-        //     apply: 'serve',
-        //     configureServer: (server) => {
-        //         server.middlewares.use((_req, res, next) => {
-        //             res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless')
-        //             res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
-        //             res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
-        //             next()
-        //         })
-        //     }
-        // },
+        {
+            // For the *-language-features extensions which use SharedArrayBuffer
+            name: 'configure-response-headers',
+            apply: 'serve',
+            configureServer: (server) => {
+                server.middlewares.use((_req, res, next) => {
+                    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless')
+                    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+                    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
+                    next()
+                })
+            }
+        },
         vsixPlugin()
     ],
     define: {
