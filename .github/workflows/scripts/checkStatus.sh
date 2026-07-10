521ad83930f19c2da89e9226fc7ae544e53ad648
Kai Salmen <kai.salmen@typefox.io>
2026-07-10 14:45:29 +0200

    Fix workflow permissions and change to preview of action

--- .github/workflows/scripts/checkStatus.sh
+++ .github/workflows/scripts/checkStatus.sh
@@ -4,9 +4,12 @@ echo -e "\nReporting env variable values:"
 
 npmDryRun=${NPM_DRY_RUN:-true}
 npmTag=${NPM_TAG:-''}
+npmVerbose=${NPM_VERBOSE:-false}
 
 echo "NPM_DRY_RUN: ${npmDryRun}"
 echo "NPM_TAG: ${npmTag}"
+echo "NPM_VERBOSE: ${npmVerbose}"
 
 echo "NPM_DRY_RUN=${npmDryRun}" >> $GITHUB_ENV
 echo "NPM_TAG=${npmTag}" >> $GITHUB_ENV
+echo "NPM_VERBOSE=${npmVerbose}" >> $GITHUB_ENV
