e8f00508f9762d4ac3cf52f014eefaa9fd105fd7
Kai Salmen <kai.salmen@typefox.io>
2026-07-13 09:18:33 +0200

    Split publish and main workflows again

--- .github/workflows/scripts/checkStatus.sh
+++ .github/workflows/scripts/checkStatus.sh
@@ -6,10 +6,12 @@ npmDryRun=${NPM_DRY_RUN:-true}
 npmTag=${NPM_TAG:-''}
 npmVerbose=${NPM_VERBOSE:-false}
 
-echo "NPM_DRY_RUN: ${npmDryRun}"
-echo "NPM_TAG: ${npmTag}"
-echo "NPM_VERBOSE: ${npmVerbose}"
-
 echo "NPM_DRY_RUN=${npmDryRun}" >> $GITHUB_ENV
 echo "NPM_TAG=${npmTag}" >> $GITHUB_ENV
 echo "NPM_VERBOSE=${npmVerbose}" >> $GITHUB_ENV
+
+echo -e "\nReporting environment variables:\n"
+
+echo "NPM_DRY_RUN: ${npmDryRun}"
+echo "NPM_TAG: ${npmTag}"
+echo "NPM_VERBOSE: ${npmVerbose}"
